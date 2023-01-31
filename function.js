e7c05006c81a0568d48faf73d301cefccf549a47
gustavo orrego <orregog566@gmail.com>
2023-01-31 11:54:52 -0300

    subiendo trabajo ticmas

---
+++ function.js
@@ -0,0 +1,29 @@
+
+document.getElementById("radio-1").onclick = function () {
+    document.getElementById("mostrar").innerHTML = "Poseo movilidad propia!";
+}
+
+document.getElementById("radio-2").onclick = function () {
+    document.getElementById("mostrar").innerHTML = "No tengo inconveniente a ser trasladado";
+}
+
+document.getElementById("radio-3").onclick = function () {
+    document.getElementById("mostrar").innerHTML = "Inglés medio";
+}
+
+document.getElementById("radio-4").onclick = function () {
+    document.getElementById("mostrar").innerHTML = "Casado";
+}
+
+document.getElementById("radio-5").onclick = function () {
+    document.getElementById("mostrar").innerHTML = "Buena predisposición a los cambios, rápido aprendizaje, trabajo en equipo, responsable, disciplinado y dedicado";
+}
+
+document.getElementById("radio-6").onclick = function () {
+    document.getElementById("mostrar").innerHTML = "Caracteristicas Personales:";
+}
+
+
+//PROBANDO LA CONSOLA
+console.log("Hola Mundo");
+console.log(8 + 9);
